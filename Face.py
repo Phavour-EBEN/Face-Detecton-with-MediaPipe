7c6a2190369c60f5fac1b24b6696f534a5378566
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-07-31 16:58:06 +0000

    Added face location data

--- Face.py
+++ Face.py
@@ -2,27 +2,36 @@ import cv2 as cv
 import time
 import utils
 import mediapipe as mp
+import numpy as np
 
-font  = cv.FONT_HERSHEY_PLAIN
+mp_face_detection = mp.solutions.face_detection
+
 cap = cv.VideoCapture(0)
-frame_counter = 0
-start_time = time.time()
+with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
+    frame_counter = 0
+    start_time = time.time()
+    font  = cv.FONT_HERSHEY_PLAIN
+    while True:
+        frame_counter  +=1
+        ret, frame = cap.read()
+        if ret is False:
+            break
 
-while True:
-    frame_counter  +=1
-    ret, frame = cap.read()
-    if ret is False:
-        break
-    fps = frame_counter/(time.time() - start_time)
-    utils.text_with_background(frame, f"FPS: {fps:.2f}", (30, 30),font)
-    
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        results = face_detection.process(rgb_frame)
+        if results.detections:
+            for face in results.detections:
+                print(face.location_data)
+        fps = frame_counter/(time.time() - start_time)
+        utils.text_with_background(frame, f"FPS: {fps:.2f}", (30, 30),font)
+        
 
-    cv.imshow('window', frame)
-    
+        cv.imshow('window', frame)
+        
 
-    key = cv.waitKey(1)
-    if key == ord('q'):
-        break
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            break
 
-cap.release()
-cv.destroyAllWindows()
+    cap.release()
+    cv.destroyAllWindows()
